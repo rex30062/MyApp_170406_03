fd72730febce41f53a09613354d628d9121fdf6d
Cclz <rex620620>
2017-04-06 15:35:05 +0800

    add handler

--- app/src/main/java/com/cclz/myapp_170406_03/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170406_03/MainActivity.java
@@ -1,8 +1,10 @@
 package com.cclz.myapp_170406_03;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,28 +16,38 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     InputStream inputStream;
+    Handler handler;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView)findViewById(R.id.textView);
+        handler=new Handler();
         new Thread(){
             @Override
             public void run(){
                 super.run();
                 try{
-                    URL url=new URL("http://www.google.com");
+                    URL url=new URL("https://udn.com/rssfeed/news/1");
                     HttpURLConnection conn =(HttpURLConnection)url.openConnection();
                     conn.setRequestMethod("GET");
                     conn.connect();
                     inputStream=conn.getInputStream();
 
                     BufferedReader br=new BufferedReader(new InputStreamReader(inputStream));
-                    StringBuilder sb=new StringBuilder();
+                    final StringBuilder sb=new StringBuilder();
                     String str;
                     while((str=br.readLine()) != null){
                         sb.append(str);
                     }
                     Log.d("MYNELog", sb.toString());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv.setText(sb.toString());
+                        }
+                    });
 
                 }catch (MalformedURLException e){
                     e.printStackTrace();
